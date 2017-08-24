9d05ead37ce1a1d854e7c3552b4e938e859981c8
PonyCui <cuis@vip.qq.com>
2017-08-24 18:10:21 +0800

    add UIView props.

---
+++ src/implementation/ios/runtime/XTRView.h
@@ -0,0 +1,13 @@
+//
+//  XTRView.h
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/8/24.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XTRView : UIView
+
+@end
---
+++ src/implementation/ios/runtime/XTRView.m
@@ -0,0 +1,21 @@
+//
+//  XTRView.m
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/8/24.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import "XTRView.h"
+
+@implementation XTRView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
