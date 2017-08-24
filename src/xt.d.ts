9ae455aa42a82c6480f54d5c67f8ce7eed1359fa
PonyCui <cuis@vip.qq.com>
2017-08-24 15:05:45 +0800

    update d.ts

--- src/xt.d.ts
+++ src/xt.d.ts
@@ -461,4 +461,31 @@ declare module 'xt' {
         makeKeyAndVisible(): void
     }
 
+    class ViewController {
+        readonly view: View
+        loadView(): void
+        viewDidLoad(): void
+        viewWillAppear(): void
+        viewDidAppear(): void
+        viewWillDisappear(): void
+        viewDidDisappear(): void
+        viewWillLayoutSubviews(): void
+        viewDidLayoutSubviews(): void
+        readonly parentViewController?: ViewController
+        childViewControllers: ViewController[]
+        addChildViewController(childController: ViewController): void
+        removeFromParentViewController(): void
+        willMoveToParentViewController(parent?: ViewController): void
+        didMoveToParentViewController(parent?: ViewController): void
+        readonly navigationController?: NavigationController
+    }
+
+    class NavigationController extends ViewController {
+        constructor(rootViewController: ViewController)
+        pushViewController(viewController: ViewController, animated?: boolean): void
+        popViewController(animated?: boolean): ViewController | undefined
+        popToViewController(viewController: ViewController, animated?: boolean): ViewController[]
+        popToRootViewController(animated?: boolean): ViewController[]
+    }
+
 }
