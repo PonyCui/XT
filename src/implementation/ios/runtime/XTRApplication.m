c7e3469c821e95ad0dd37109ae7b8ee119118ccf
PonyCui <cuis@vip.qq.com>
2017-08-24 16:35:03 +0800

    add ios.

---
+++ src/implementation/ios/runtime/XTRApplication.m
@@ -0,0 +1,13 @@
+//
+//  XTRApplication.m
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/8/24.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import "XTRApplication.h"
+
+@implementation XTRApplication
+
+@end
