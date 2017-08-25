df1ac53268013a51d9c30a8f87f3e762ac4ef8f8
PonyCui <cuis@vip.qq.com>
2017-08-25 14:30:55 +0800

    Add View and Window to iOS.

---
+++ src/implementation/ios/runtime/XTRWindow.h
@@ -0,0 +1,13 @@
+//
+//  XTRWindow.h
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/8/25.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XTRWindow : UIWindow
+
+@end
---
+++ src/implementation/ios/runtime/XTRWindow.m
@@ -0,0 +1,21 @@
+//
+//  XTRWindow.m
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/8/25.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import "XTRWindow.h"
+
+@implementation XTRWindow
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
