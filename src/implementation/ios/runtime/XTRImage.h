5f388fc69effacc6f5b9d69dbfd79442f9f9dcc7
PonyCui <cuis@vip.qq.com>
2017-08-28 18:41:21 +0800

    add ImageView.

---
+++ src/implementation/ios/runtime/XTRImage.h
@@ -0,0 +1,13 @@
+//
+//  XTRImage.h
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/8/28.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XTRImage : UIImage
+
+@end
---
+++ src/implementation/ios/runtime/XTRImageView.m
@@ -0,0 +1,21 @@
+//
+//  XTRImageView.m
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/8/28.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import "XTRImageView.h"
+
+@implementation XTRImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
