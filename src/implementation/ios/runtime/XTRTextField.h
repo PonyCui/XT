a0f9a6d7f5005b7a1a98fd97d44de8b58bf6ba2b
PonyCui <cuis@vip.qq.com>
2017-09-14 10:48:33 +0800

    add textfield props.

---
+++ src/implementation/ios/runtime/XTRTextField.h
@@ -0,0 +1,13 @@
+//
+//  XTRTextField.h
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/9/13.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import "XTRView.h"
+
+@interface XTRTextField : XTRView
+
+@end
---
+++ src/implementation/ios/runtime/XTRTextField.m
@@ -0,0 +1,21 @@
+//
+//  XTRTextField.m
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/9/13.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import "XTRTextField.h"
+
+@implementation XTRTextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
