b519b95fb69b65342b90056ae4056ae8ab047b5c
PonyCui <cuis@vip.qq.com>
2017-09-27 17:49:46 +0800

    add Plugin, ios.

---
+++ samples/iOS/FOOPlugin/FOOPlugin.m
@@ -0,0 +1,13 @@
+//
+//  FOOPlugin.m
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/9/27.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import "FOOPlugin.h"
+
+@implementation FOOPlugin
+
+@end
