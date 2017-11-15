d819a7ae71a0a1a03797d5a9875b9471d88432f3
PonyCui <cuis@vip.qq.com>
2017-11-15 16:23:19 +0800

    add TextMeasurer, ios.

---
+++ ios/runtime/XTRTextMeasurer.h
@@ -0,0 +1,13 @@
+//
+//  XTRTextMeasurer.h
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/11/15.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XTRTextMeasurer : NSObject
+
+@end
---
+++ ios/runtime/XTRTextMeasurer.m
@@ -0,0 +1,13 @@
+//
+//  XTRTextMeasurer.m
+//  XTSample
+//
+//  Created by 崔明辉 on 2017/11/15.
+//  Copyright © 2017年 UED Center, YY Inc. All rights reserved.
+//
+
+#import "XTRTextMeasurer.h"
+
+@implementation XTRTextMeasurer
+
+@end
