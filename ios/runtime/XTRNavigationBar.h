92eaa10c840f6bde2b9126cbace03b77664e4ed2
PonyCui <cuis@vip.qq.com>
2018-01-03 18:09:50 +0800

    add navigationbar.

---
+++ ios/runtime/XTRNavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  XTRNavigationBar.h
+//  XTSample
+//
+//  Created by 崔明辉 on 2018/1/3.
+//  Copyright © 2018年 UED Center, YY Inc. All rights reserved.
+//
+
+#import "XTRView.h"
+
+@interface XTRNavigationBar : XTRView
+
+@end
