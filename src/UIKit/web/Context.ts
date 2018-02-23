65826fa1ec838c1b396ca365b968b3448c864fcc
PonyCui <cuis@vip.qq.com>
2018-02-23 10:08:23 +0800

    handle orientation.

--- src/UIKit/web/Context.ts
+++ src/UIKit/web/Context.ts
@@ -45,6 +45,7 @@ export class Context implements Releasable {
     }
 
     private application?: Application = undefined
+    private attachNode?: HTMLElement = undefined
 
     private eval(code: string) {
         (window as any).__XT_CONTEXT__ = this;
@@ -58,19 +59,42 @@ export class Context implements Releasable {
 
     public attachTo(node: HTMLElement | undefined) {
         if (this.application === undefined) { return }
-        (node || document.body).appendChild(this.application.rootElement)
-        if ((node === undefined || node === document.body) && this.application.keyWindow) {
+        this.attachNode = node || document.body;
+        this.attachNode.appendChild(this.application.rootElement)
+        if (this.attachNode === document.body && this.application.keyWindow) {
             this.application.keyWindow.isBody = true
         }
+        let previousOrientation = document.body.clientWidth > document.body.clientHeight ? 1 : 0
+        let inputMode = false
         window.addEventListener("resize", () => {
-            this.resize()
+            if (document.activeElement instanceof HTMLInputElement ||
+                document.activeElement instanceof HTMLTextAreaElement) {
+                inputMode = true
+                return;
+            }
+            else if (inputMode) {
+                if (this.attachNode && this.application && this.application.rootElement) {
+                    this.attachNode.removeChild(this.application.rootElement)
+                    this.attachNode.appendChild(this.application.rootElement)
+                    this.resize()
+                }
+                inputMode = false
+                return;
+            }
+            let currentOrientation = document.body.clientWidth > document.body.clientHeight ? 1 : 0
+            if (currentOrientation != previousOrientation) {
+                window.location.reload()
+            }
+            else if (this.attachNode && this.application && this.application.rootElement) {
+                this.resize()
+            }
         })
         this.resize()
     }
 
     public resize() {
-        if (this.application === undefined || this.application.keyWindow === undefined) { return }
-        this.application.keyWindow.frame = RectMake(0.0, 0.0, this.application.rootElement.clientWidth, this.application.rootElement.clientHeight)
+        if (this.application === undefined || this.application.keyWindow === undefined || this.attachNode === undefined) { return }
+        this.application.keyWindow.frame = RectMake(0.0, 0.0, this.attachNode.clientWidth, this.attachNode.clientHeight)
     }
 
 }
