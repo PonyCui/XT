5e0debc8825d09ba35e0f6402afd576e768ac20c
PonyCui <cuis@vip.qq.com>
2018-03-01 17:31:07 +0800

    add RefreshControl and LoadMoreControl, iOS.

---
+++ ios/Sources/UIKit/XTUIRefreshControl.h
@@ -0,0 +1,13 @@
+//
+//  XTUIRefreshControl.h
+//  XTSample
+//
+//  Created by 崔明辉 on 2018/3/1.
+//  Copyright © 2018年 UED Center, YY Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XTUIRefreshControl : UIRefreshControl
+
+@end
---
+++ ios/Sources/UIKit/XTUIRefreshControl.m
@@ -0,0 +1,21 @@
+//
+//  XTUIRefreshControl.m
+//  XTSample
+//
+//  Created by 崔明辉 on 2018/3/1.
+//  Copyright © 2018年 UED Center, YY Inc. All rights reserved.
+//
+
+#import "XTUIRefreshControl.h"
+
+@implementation XTUIRefreshControl
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
